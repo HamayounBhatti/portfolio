1995d5a890427da62bf5982c8b16e6cc76cac080
HamayounBhatti <88651217+HamayounBhatti@users.noreply.github.com>
2025-01-13 13:57:19 +0500

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/magic-ike/leanfolio'
+      href='http://hamayounbhatti.online/?i=2'
       target='_blank'
       rel='noreferrer'
       className='link footer__link'
